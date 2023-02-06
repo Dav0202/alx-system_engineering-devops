fb1356a7fae06fd33cdcbeb199ed866d28d657ee
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-02-06 11:28:05 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,9 @@
-
+# Web Stack debugging increase limit of open files
+exec { 'fix--for-nginx':
+  environment => ['DIR=/etc/default/nginx',
+                  'OLD=ULIMIT="-n 15"',
+                  'NEW=ULIMIT="-n 15000"'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}
